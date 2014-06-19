a2834bfb0752310f957c572ddd6919baa21f7a80
Kaoru Maeda <maeda@lepidum.co.jp>
2014-06-19 18:49:57 +0900

    Updated to HPACK-08 test cases

--- spec/hpack_test_spec.rb
+++ spec/hpack_test_spec.rb
@@ -3,26 +3,27 @@ require 'json'
 
 describe HTTP2::Header do
   folders = %w[
-    go-hpack
-    haskell-http2-diff
-    haskell-http2-diff-huffman
-    haskell-http2-linear
-    haskell-http2-linear-huffman
-    haskell-http2-naive
-    haskell-http2-naive-huffman
-    haskell-http2-static
-    haskell-http2-static-huffman
-    hyper-hpack
+    #go-hpack
+    #haskell-http2-diff
+    #haskell-http2-diff-huffman
+    #haskell-http2-linear
+    #haskell-http2-linear-huffman
+    #haskell-http2-naive
+    #haskell-http2-naive-huffman
+    #haskell-http2-static
+    #haskell-http2-static-huffman
+    #hyper-hpack
     nghttp2
     nghttp2-16384-4096
     nghttp2-change-table-size
-    node-http2-hpack
+    #node-http2-hpack
   ]
 
   context "Decompressor" do
     folders.each do |folder|
+      path = File.expand_path("hpack-test-case/#{folder}", File.dirname(__FILE__))
+      Dir.exists?(path) or next
       context "#{folder}" do
-        path = File.expand_path("hpack-test-case/#{folder}", File.dirname(__FILE__))
         Dir.foreach(path) do |file|
           next if file !~ /\.json/
           it "should decode #{file}" do
